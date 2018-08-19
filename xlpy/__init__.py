d9166220859cd69c300695275f11523b5336bca4
gretongertech <42519387+gretongertech@users.noreply.github.com>
2018-08-20 02:56:33 +0700

    Create __init__.py

---
+++ xlpy/__init__.py
@@ -0,0 +1,10 @@
+from .config import Config
+from .client import XL
+
+__copyright__       = 'Copyright 2018 by Gretonger Tech'
+__version__         = '3.0.0'
+__author__          = 'Gretonger Tech'
+__author_email__    = 'gretonger.id@gmail.com'
+__url__             = 'https://github.com/gretongerid'
+
+__all__ = ['Config','XL']
